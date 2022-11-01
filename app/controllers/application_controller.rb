be776dbdd10f61a6273c646e47207f6ede1f8a49
Vincent Makokha <makokhavomondi@gmail.com>
2022-11-01 18:42:02 +0300

    Completed Assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, "application/json"
 
   # add routes
+  get '/bakeries' do 
+    Bakery.all.to_json
+  end
 
+  get '/bakeries/:id' do
+    Bakery.find(params[:id]).to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    BakedGood.all.sort_by {|good| -good.price}.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    max_price = BakedGood.maximum(:price).to_json
+    BakedGood.find_by(price: max_price).to_json
+  end
 end
